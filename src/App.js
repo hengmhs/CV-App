855c705141c4eff8764ad18bdd6eadb0ee461cd4
hengmhs <31433170+hengmhs@users.noreply.github.com>
2021-06-05 15:09:25 +0800

    Style and finish General Info section

--- src/App.js
+++ src/App.js
@@ -11,23 +11,25 @@ class App extends React.Component {
   render() {
     return (
       <div className = 'container'>
-      <h1>General Information</h1>
-        <Section/>
+      <h2>General Information</h2>
+        <GenSection/>
+      <h2>Education</h2>
+        <GenSection/>
       </div>
     );
   }
 
 }
 
-class Section extends React.Component {
+class GenSection extends React.Component {
   constructor(props){
     super(props);
     this.state = {
       isEditable: true,
-      firstName: null,
-      lastName: null,
-      email: null,
-      phoneNum: null
+      firstName: 'Jeffret',
+      lastName: 'Sanders',
+      email: 'jeffers@gmail.com',
+      phoneNum: '91231234'
     };
     this.changeEditStatus = this.changeEditStatus.bind(this);
     this.handleChange = this.handleChange.bind(this);
@@ -52,39 +54,40 @@ class Section extends React.Component {
             <div className='fieldRow'>
               <div>
                 <div>First Name: </div>
-                <div><input type="text" id='firstName' onChange = {this.handleChange} /></div>  
+                <div><input type="text" id='firstName' onChange = {this.handleChange} value = {this.state.firstName} /></div>  
               </div>
               <div>
                 <div>Last Name: </div>
-                <div><input type="text" id='lastName' onChange = {this.handleChange} /></div>  
+                <div><input type="text" id='lastName' onChange = {this.handleChange} value = {this.state.lastName} /></div>  
               </div>
             </div>
             <div className='fieldRow'>
             <div>
                 <div>Email: </div>
-                <div><input type="text" id='email' onChange = {this.handleChange} /></div>  
+                <div><input type="text" id='email' onChange = {this.handleChange} value = {this.state.email} /></div>  
               </div>
               <div>
                 <div>Phone Number: </div>
-                <div><input type="text" id='phoneNum' onChange = {this.handleChange} /></div>  
+                <div><input type="text" id='phoneNum' onChange = {this.handleChange} value = {this.state.phoneNum} /></div>  
               </div>
             </div>
-            <div className='editBtnDiv'>
-              <button class='editBtn' onClick={this.changeEditStatus}>Edit</button>
+            <div className='submitBtnDiv'>
+              <button className='submitBtn' onClick={this.changeEditStatus}>Submit</button>
             </div>
         </div>
       );
     }
     else{
       return (
-        <div className='section'>   
-            <div>First Name: {this.state.firstName} </div>
-            <div>Last Name: {this.state.lastName} </div>
-            <div>Email: {this.state.email} </div>
-            <div>Phone Number: {this.state.phoneNum} </div>
-        <div className='editBtnDiv'>
-          <button className='editBtn' onClick={this.changeEditStatus}>Edit</button>
-        </div>
+        <div className='editsection'>
+            <div className='printsection'>
+              <div><strong>{this.state.firstName} {this.state.lastName}</strong></div>
+              <div>📧 {this.state.email} </div>
+              <div>📞 {this.state.phoneNum} </div>
+            </div>   
+          <div className='editBtnDiv'>
+            <button className='editBtn' onClick={this.changeEditStatus}>🖊️</button>
+          </div>
     </div>
       )
     }
