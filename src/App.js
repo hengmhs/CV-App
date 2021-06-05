c1e9cc438bf5d522da4a80fe9f61b9a6c9c3eee5
hengmhs <31433170+hengmhs@users.noreply.github.com>
2021-06-05 14:47:44 +0800

    Lift up state for Section

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
-        <Section/>
+      <div className = 'container'>
+      <h1>General Information</h1>
         <Section/>
       </div>
     );
@@ -24,8 +24,13 @@ class Section extends React.Component {
     super(props);
     this.state = {
       isEditable: true,
+      firstName: null,
+      lastName: null,
+      email: null,
+      phoneNum: null
     };
     this.changeEditStatus = this.changeEditStatus.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   };
 
   changeEditStatus(event) {
@@ -33,60 +38,58 @@ class Section extends React.Component {
     this.setState({isEditable: !isEditable})
   }
 
-  render() {
-    return (
-      <div className='section'>   
-          <div className='fieldRow'>
-            <EditField isEditable = {this.state.isEditable} labelName = 'First Name'/>
-            <EditField isEditable = {this.state.isEditable} labelName = 'Last Name'/>
-          </div>
-          <div className='fieldRow'>
-            <EditField isEditable = {this.state.isEditable} labelName = 'Email'/>
-            <EditField isEditable = {this.state.isEditable} labelName = 'Phone Number'/>
-          </div>
-          <div className='editBtnDiv'>
-            <button class='editBtn' onClick={this.changeEditStatus}>Edit</button>
-          </div>
-      </div>
-    );
-  }
-}
-
-class EditField extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      value: '',
-    };
-    this.handleChange = this.handleChange.bind(this);
-  };
-
   handleChange(event) {
-    this.setState({value: event.target.value});
+    const name = event.target.id;
+    this.setState({[name]: event.target.value});
+    console.log(this.state[name]);
   }
 
   render() {
-    const isEditable = this.props.isEditable;
-    const labelName = this.props.labelName;
-    if(isEditable) {
-      return(
-        <div className="EditField">
-          <div>{labelName}:</div>
-          <div><input type="text" value={this.state.value} onChange = {this.handleChange} /></div>  
-        </div>
-      ) 
-    } else {
+    let isEditable = this.state.isEditable;
+    if(isEditable){
       return (
-        <div className="EditField">
-            <div> {labelName}: {this.state.value} </div>
+        <div className='section'>   
+            <div className='fieldRow'>
+              <div>
+                <div>First Name: </div>
+                <div><input type="text" id='firstName' onChange = {this.handleChange} /></div>  
+              </div>
+              <div>
+                <div>Last Name: </div>
+                <div><input type="text" id='lastName' onChange = {this.handleChange} /></div>  
+              </div>
+            </div>
+            <div className='fieldRow'>
+            <div>
+                <div>Email: </div>
+                <div><input type="text" id='email' onChange = {this.handleChange} /></div>  
+              </div>
+              <div>
+                <div>Phone Number: </div>
+                <div><input type="text" id='phoneNum' onChange = {this.handleChange} /></div>  
+              </div>
+            </div>
+            <div className='editBtnDiv'>
+              <button class='editBtn' onClick={this.changeEditStatus}>Edit</button>
+            </div>
         </div>
       );
     }
-    
+    else{
+      return (
+        <div className='section'>   
+            <div>First Name: {this.state.firstName} </div>
+            <div>Last Name: {this.state.lastName} </div>
+            <div>Email: {this.state.email} </div>
+            <div>Phone Number: {this.state.phoneNum} </div>
+        <div className='editBtnDiv'>
+          <button className='editBtn' onClick={this.changeEditStatus}>Edit</button>
+        </div>
+    </div>
+      )
+    }
   }
-
 }
-
 
 
 export default App;
