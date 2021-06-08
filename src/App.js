67a773ee67fc500dd41025d483e2c3ba45356ced
hengmhs <31433170+hengmhs@users.noreply.github.com>
2021-06-08 20:30:53 +0800

    Add Card functionality for Education

--- src/App.js
+++ src/App.js
@@ -4,23 +4,37 @@ import GenSection from './components/GenSection.js'
 import EduSection from './components/EduSection.js'
 
 class App extends React.Component {
+  
   constructor(props){
     super(props);
     this.state = {
+      education: [<EduSection/>],
     };
-  };
+    this.addEdu = this.addEdu.bind(this);
+    }; 
+  
+   
+    addEdu(){
+      console.log('test');
+      const education = this.state.education.concat(<GenSection/>);
+      console.log(education);
+      this.setState({education: education});
+    };
+
+
 
   render() {
     return (
       <div className = 'container'>
       <h2>General Information</h2>
         <GenSection/>
+        
       <h2>Education</h2>
-        <EduSection/>
+        {this.state.education}
+        <button onClick={this.addEdu}> Add </button>
       </div>
     );
   }
-
-}
+};
 
 export default App;
